f9083cb68b0c61feebc94f4359b9c777a85715dc
zwp <2864541164@qq.com>
2020-12-28 13:58:19 +0800

    Update pb_interface.py

--- pb_interface.py
+++ pb_interface.py
@@ -8,7 +8,7 @@ def generate_interface(service):
     i_file_name = 'I' + file_name
     text = common.generate_header_comment(file_name, 'h')
     text = text + '#import \"HXNetworkServiceComponent.h\"\n#import \"%s.h\"\n\nNS_ASSUME_NONNULL_BEGIN\n\n@interface ' \
-                  '%s : HXNetworkServiceComponent<%s>\n\n-(void)customInitSelf;\n\n@end\n\nNS_ASSUME_NONNULL_END' % (
+                  '%s : HXNetworkServiceComponent<%s>\n\n@end\n\nNS_ASSUME_NONNULL_END' % (
                i_file_name, file_name, i_file_name)
 
     return file_name, text
