302fc8bcc72e4650b222b9873e7b490bb3c95367
zengweipeng <zengweipeng@xinyu100.com>
2020-12-28 14:52:56 +0800

    1

---
+++ demo/Network/Version/HXVersionServiceComponent.h
@@ -0,0 +1,20 @@
+//
+//  HXVersionServiceComponent.h
+//  demo
+//
+//  Created by zwp on 2020/12/28.
+//  Copyright © 2020年 XY. All rights reserved.
+//
+
+//⚠️ 该文件由脚本自动生成，请不要手动更改
+
+#import "HXNetworkServiceComponent.h"
+#import "IHXVersionServiceComponent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HXVersionServiceComponent : HXNetworkServiceComponent<IHXVersionServiceComponent>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ demo/Network/Version/IHXVersionServiceComponent.h
@@ -0,0 +1,27 @@
+//
+//  IHXVersionServiceComponent.h
+//  demo
+//
+//  Created by zwp on 2020/12/28.
+//  Copyright © 2020年 XY. All rights reserved.
+//
+
+//⚠️ 该文件由脚本自动生成，请不要手动更改
+
+#import "HXNetwork.h"
+#import "VersionExt.pbobjc.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol IHXVersionServiceComponent <NSObject>
+
+-(void)getLastVersion:(PB3GetLastVersionReq *)req complete:(CompleteHandler)complete;
+
+-(void)installReport:(PB3InstallReportReq *)req complete:(CompleteHandler)complete;
+
+-(void)getViewHideInfo:(PB3GetViewHideInfoReq *)req complete:(CompleteHandler)complete;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
